d3ea5a8939033f428d7514deadc4c187c11808c2
RimmaPazyapova <rimmaraziapova@yandex.ru>
2023-03-24 21:33:35 +0500

    добавлены тесты

--- src/test/java/FelineSpyTest.java
+++ src/test/java/FelineSpyTest.java
@@ -1,8 +1,8 @@
 import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
@@ -10,16 +10,16 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
-public class FelineMockTest {
+public class FelineSpyTest {
 
-    @Mock
+    @Spy
     Feline feline;
 
     @Test
     public void getEatMeatReturnsForFeline() throws Exception {
-        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-        List<String> expectedEatMeat = feline.eatMeat();
-        List<String> actualEatMeat = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> expectedEatMeat = List.of("Животные", "Птицы", "Рыба");
+        List<String> actualEatMeat = feline.eatMeat();
         assertEquals(expectedEatMeat, actualEatMeat);
     }
 
